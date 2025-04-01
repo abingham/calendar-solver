3237d1a9eb4b9188296812ce663515e9a2f26a72
Austin Bingham <austin.binghm@pm.me>
2025-04-01 08:43:10 +0200

    Updated docs.

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
+from itertools import chain
+from typing import Iterable
+import more_itertools
+import numba
 import numba.typed.typedlist
 import numpy
-import more_itertools
-from numba.typed.typedlist import List
 
 
-def orientations(piece: numpy.ndarray) -> tuple[numpy.ndarray, ...]:
-    """Return all orientations of a piece."""
+def orientations(piece: numpy.ndarray) -> Iterable[numpy.ndarray]:
+    """Return all orientations of a piece.
+    
+    This rotates a piece and its flipped version, and returns all unique
+    orientations. 
+    """
 
     def rotated(p):
-        return [
-            p,
-            numpy.rot90(p),
-            numpy.rot90(p, 2),
-            numpy.rot90(p, 3),
-        ]
+        "Get all rotations of a piece: 0, 90, 180, and 270 degrees."
+        yield p
+        yield numpy.rot90(p)
+        yield numpy.rot90(p, 2)
+        yield numpy.rot90(p, 3)
 
-    return tuple(
-        piece.copy()
-        for piece in more_itertools.unique_everseen(
-            rotated(piece) + rotated(numpy.flip(piece, axis=0)),
-            key=lambda x: x.tobytes(),
-        )
+    return more_itertools.unique_everseen(
+        chain(rotated(piece), rotated(numpy.flip(piece, axis=0))),
+        key=lambda x: x.tobytes(),
     )
 
 
@@ -31,20 +33,38 @@ def count_zeros(arr: numpy.ndarray) -> int:
 
 
 def solve(board, buckets):
-    new_buckets = List()
+    """Solve the board with the given buckets.
+    
+    Args:
+        board: The 2D numpy array representing the board. It should have zeroes in any empty spaces, and any positive integer elsewhere.
+        buckets: A list of lists, where each inner list contains pieces (numpy arrays) that can be placed on the board.
+
+    Returns: True if the board is solved, False otherwise. The `board` argument will contain the final state of the board, solved or not.
+    """
+    new_buckets = numba.typed.typedlist.List()
     for bucket in buckets:
-        new_bucket = List()
+        new_bucket = numba.typed.typedlist.List()
         for piece in bucket:
             new_bucket.append(piece)
         new_buckets.append(new_bucket)
-    return _solve(board, new_buckets)
+    result = _solve(board, new_buckets)
+
+    if result == 0:
+        assert count_zeros(board) == 0, "The board should have no zeros if it's solved."
+
+    return result == 0
 
 
 @numba.njit
 def _solve(board, buckets, counter=0):
-    """Solve the board with the given buckets."""
+    """Numba-based recursive function to solve the board.
+
+    Returns:
+        0 if the board is solved, otherwise returns the number of total iterations.
+    """
     if not buckets:
-        return False
+        # TODO: This is a bit wrong. If we're called initially with no buckets, we return 0 here indicating "solved" when in fact we've solved nothing.
+        return counter
 
     for piece in buckets[0]:
         for i in range(0, board.shape[0] - piece.shape[0] + 1):
@@ -62,7 +82,7 @@ def _solve(board, buckets, counter=0):
                     if post_zeros == 0:
                         return 0
 
-                    counter =_solve(board, buckets[1:], counter)
+                    counter = _solve(board, buckets[1:], counter)
                     if counter == 0:
                         return 0
 
@@ -97,7 +117,10 @@ PIECES = [
     numpy.array([[1, 1, 1], [1, 0, 1]], dtype=numpy.int8),
 ]
 
-BUCKETS = [orientations(piece * index) for index, piece in enumerate(PIECES, start=2)]
+BUCKETS = [
+    [o.copy() for o in orientations(piece * index)]
+    for index, piece in enumerate(PIECES, start=2)
+]
 
 solved = solve(BOARD, BUCKETS)
 print("solved:", solved)
